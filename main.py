f20502bf93922d89a1d88fd5d3a779d6af2bb7bd
smit8585 <smitgpatel8585@gmail.com>
2021-10-24 12:12:53 +0530

    tried solving error

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from pmdarima import auto_arima
 import warnings
 from statsmodels.tsa.arima.model import ARIMA
 
-data = 0
+# data = 0
 @st.cache
 def pre_process():
-    data = pd.read_csv("data\chennai.csv", index_col='Date', parse_dates=True)
-    data = data.dropna()
+    data_set = pd.read_csv("data\chennai.csv", index_col='Date', parse_dates=True)
+    data_set = data_set.dropna()
 
-    def ad_test(dataset):
-        datatest = adfuller(dataset, autolag='AIC')
+    # def ad_test(dataset):
+    #     datatest = adfuller(dataset, autolag='AIC')
 
     warnings.filterwarnings("ignore")
 
-    train = data.iloc[:-1000]
-    test = data.iloc[-1000:]
+    train = data_set.iloc[:-1000]
+    test = data_set.iloc[-1000:]
 
     return train,test
 
